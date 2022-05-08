001c94fa514cdf4519b6ba29db41446a831d8712
satyajeetramnit <1906130@kiit.ac.in>
2022-05-08 12:37:07 +0530

    Codes Rectified

--- LoveBabbar/12_graph/20_total_no_of_spanning_tree_in_graph.cpp
+++
@@ -1,5 +0,0 @@
-/*
-    link: https://www.geeksforgeeks.org/total-number-spanning-trees-graph/
-    [nice way to find total no. of spanning tree]
-    [also called Kirchhoff’s Theorem]
-*/
--- LoveBabbar/12_graph/43_Minimize_Cash_Flow_among_a_given_set_of_friends_who_have_borrowed_money_from_each_other.cpp
+++
@@ -1,5 +0,0 @@
-/*
-    link [greedy]: https://www.geeksforgeeks.org/minimize-cash-flow-among-given-set-friends-borrowed-money/
-
-    ref: greedy/11/min_cash_flow...
-*/
